2b42b3f7bd0794a1c7f75683b8dc66993fb7e2b6
rabelais88 <sungryeolp@gmail.com>
2018-08-20 17:08:02 +0900

    update

--- src/ch4.js
+++ src/ch4.js
@@ -6,15 +6,57 @@ let myData = [];
 for (let i = 0; i < 20;i ++) {
   myData.push([rand(50),rand(250)]);
 }
-const maxWidth = 250;
-const maxHeight = 250;
+const svgWidth = 800;
+const svgHeight = 500;
 
 const lowestX = d3.min(myData, d => d[0]);
 const highestX = d3.max(myData, d => d[0])
-const scaleX = d3.scaleLinear().domain([lowestX, highestX]).range([0, maxWidth]);
+const scaleX = d3.scaleLinear().domain([lowestX, highestX]).range([100, svgWidth - 100]);
 
 const lowestY = d3.min(myData, d => d[1])
 const highestY = d3.max(myData, d => d[1])
-const scaleY = d3.scaleLinear().domain([lowestY, highestY]).range([0, maxHeight]);
+const scaleY = d3.scaleLinear().domain([lowestY, highestY]).range([100, svgHeight - 100]);
 
 // ~p.145
+
+const svg = d3.select('body')
+  .append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight)
+  .style('background-color', 'lightgray');
+
+const dots = svg.selectAll('circle')
+  .data(myData)
+  .enter()
+  .append('circle')
+  .attr('r', d => Math.sqrt(svgHeight - d[1]))
+  .attr('cx', d => scaleX(d[0]))
+  .attr('cy', d => scaleY(d[1]))
+  .attr('fill', 'black');
+/**
+ * D3’s call() function takes the incoming selection, as received from the prior link in the chain,
+ * and hands that selection off to any function.
+ * In this case, the selection is our new g group element.
+ * Although the g isn’t strictly necessary,
+ * we are using it because the axis function is about to generate lots of crazy lines and numbers,
+ * and it’s nice to contain all those elements within a single group object.
+ * call() hands off g to the xAxis function, so our axis is generated within g.
+ */
+
+// raw syntax for drawing axis with .call()
+const xAxis = d3.axisBottom().scale(scaleX).ticks(5); // .tick() is still available! in a different usage tho
+
+const yAxis = d3.axisRight().scale(scaleY).tickValues([0, 50, 100, 250]);
+
+svg.append('g')
+  .attr('class', 'axis')
+  .call(xAxis);
+
+svg.append('g')
+  .attr('class', 'axis')
+  .call(xAxis)
+  .attr('transform')
+
+svg.append('g')
+  .attr('class', 'axis')
+  .call(yAxis);
---
+++ src/ch5.js
@@ -0,0 +1,51 @@
+// scaling time & xy plotting
+
+const parseTime = d3.timeParse("%m/%d/%y");
+const rowConverter = (d) => { return {
+  Date: parseTime(d.Date),
+  Amount: parseInt(d.Amount)
+}};
+
+d3.csv('times.csv', rowConverter).then(scaleTime)
+
+function scaleTime(timesData) {
+  console.table(timesData)
+  const svgWidth = 800;
+  const svgHeight = 500;
+  const padding = 100;
+
+  const xScale = d3.scaleTime()
+    .domain([
+    d3.min(timesData, d => d.Date),
+    d3.max(timesData, d => d.Date) ])
+    .range([padding, svgWidth - padding]);
+  
+  const yScale = d3.scaleLinear()
+    .domain([d3.min(timesData, d => d.Amount), d3.max(timesData, d => d.Amount)])
+    .range([padding, svgHeight - padding]);
+
+  const svg = d3.select('body').append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+  
+  const circles = svg.selectAll('g')
+    .data(timesData)
+    .enter()
+    .append('g')
+    .append('circle')
+    .attr('r', '5px')
+    .attr('fill', 'black')
+    .attr('cx', d => xScale(d.Date))
+    .attr('cy', d => yScale(d.Amount))
+  
+
+  const texts = svg.selectAll('text')
+    .data(timesData)
+    .enter()
+    .append('text')
+    .text(d => d3.timeFormat('%Y %m %d')(d.Date)) // or just const tf = d3.timeFormat('%y %m %d'); tf(d.Date);
+    .attr('fill', 'black')
+    .attr('x', d => xScale(d.Date))
+    .attr('y', d => yScale(d.Amount))
+
+}
